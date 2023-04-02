46819bdac23d952bcc09bfa5f5eb4351b74c803f
Meerim Sadykbekova <sadykbekova.m@gmail.com>
2023-04-02 12:27:04 +0600

    Completed all tasks.

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max, temp;
+        temp = first > second ? first : second;
+        max = temp > third ? temp : third;
+
+        System.out.println(max);
     }
 }
